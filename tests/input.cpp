2fe014bdd2f5d29a3d2334e0955000875a4d5f2a
Ivan Koveshnikov <ikoveshnik@gmail.com>
2016-08-16 21:06:59 +0500

    start solving task

---
+++ tests/input.cpp
@@ -0,0 +1,13 @@
+ #define BOOST_TEST_DYN_LINK
+ #define BOOST_TEST_MODULE "OrionCore_CmdLine"
+
+#include <boost/test/unit_test.hpp>
+
+#include "input.h"
+
+
+BOOST_AUTO_TEST_CASE( no_params )
+{
+\
+    BOOST_CHECK(true);
+}
