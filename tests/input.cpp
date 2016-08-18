814708b1a26d5deb0c73203d5bae0d30c8eb1df6
Ivan Koveshnikov <ikoveshnik@gmail.com>
2016-08-19 00:32:36 +0500

    tests: add more test cases

--- tests/input.cpp
+++ tests/input.cpp
@@ -1,37 +1,115 @@
  #define BOOST_TEST_DYN_LINK
- #define BOOST_TEST_MODULE "OrionCore_CmdLine"
+ #define BOOST_TEST_MODULE "TG_input"
 
 #include <boost/test/unit_test.hpp>
 
 #include "input.h"
+#include "tg_utils.h"
+#include "tests_config.h"
 
 
-BOOST_AUTO_TEST_CASE( input_data )
+
+BOOST_AUTO_TEST_CASE( good_data )
 {
-    input_t in = {4, 2, 2, 2, 2, 1, 4, 1, 1, 4, 3, 1, 2, 1, 3, 3, 2, 4, 2};
+    InputData data (sample);
 
-    InputData data (in);
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::Ok);
+    BOOST_CHECK_EQUAL(data.GetTableSize(), SAMPLE_TABLE_SIZE);
+    BOOST_CHECK_EQUAL(data.GetBallCount(), SAMPLE_BALLS_COUNT);
+    BOOST_CHECK_EQUAL(data.GetWallCount(), SAMPLE_WALLS_COUNT);
 
-    BOOST_CHECK_EQUAL(data.IsValid(), true);
-    BOOST_CHECK_EQUAL(data.GetTableSize(), 4);
-    BOOST_CHECK_EQUAL(data.GetBallCount(), 2);
-    BOOST_CHECK_EQUAL(data.GetWallCount(), 2);
+    std::vector <coordinates_t>  balls_sample;
+    balls_sample.push_back({SAMPLE_BALL_1});
+    balls_sample.push_back({SAMPLE_BALL_2});
 
-    coordinates_t ball_1 (2,2);
-    coordinates_t ball_2 (1,4);
+    std::vector <coordinates_t> balls = data.GetBalls();
+    BOOST_CHECK_EQUAL_COLLECTIONS(balls.begin(), balls.end(),
+                                  balls_sample.begin(), balls_sample.end());
 
-    coordinates_t hole_1 (1,1);
-    coordinates_t hole_2 (4,3);
+    std::vector <coordinates_t>  holes_sample;
+    holes_sample.push_back({SAMPLE_HOLE_1});
+    holes_sample.push_back({SAMPLE_HOLE_2});
 
-    BOOST_CHECK(data.GetBalls().at(0) == ball_1);
-    BOOST_CHECK(data.GetBalls().at(1) == ball_2);
-    BOOST_CHECK(data.GetHoles().at(0) == hole_1);
-    BOOST_CHECK(data.GetHoles().at(1) == hole_2);
+    std::vector <coordinates_t>  holes = data.GetHoles();
+    BOOST_CHECK_EQUAL_COLLECTIONS(holes.begin(), holes.end(),
+                                  holes_sample.begin(), holes_sample.end());
 
-    wall_coordinates_t wall_1 {{1,2}, {1,3}};
-    wall_coordinates_t wall_2 {{3,2}, {4,2}};
+    std::vector <wall_coordinates_t> walls_sample;
+    walls_sample.push_back(wall_coordinates_t(SAMPLE_WALL_1));
+    walls_sample.push_back(wall_coordinates_t(SAMPLE_WALL_2));
 
-    BOOST_CHECK(data.GetWalls().at(0) == wall_1);
-    BOOST_CHECK(data.GetWalls().at(1) == wall_2);
+    std::vector<wall_coordinates_t> walls = data.GetWalls();
+    BOOST_CHECK_EQUAL_COLLECTIONS(walls.begin(), walls.end(),
+                                  walls_sample.begin(), walls_sample.end());
 
+}
+
+BOOST_AUTO_TEST_CASE (incomplite_1)
+{
+    InputData data (sample_incomplite_1);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::IncompliteData);
+}
+
+BOOST_AUTO_TEST_CASE (incomplite_2)
+{
+    InputData data (sample_incomplite_2);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::IncompliteData);
+}
+
+BOOST_AUTO_TEST_CASE (too_long)
+{
+    InputData data (sample_too_long);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::TooLongData);
+}
+
+BOOST_AUTO_TEST_CASE (invalid_1)
+{
+    InputData data (sample_invalid_1);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::InvalidCoordinates);
+}
+
+BOOST_AUTO_TEST_CASE (invalid_2)
+{
+    InputData data (sample_invalid_1);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::InvalidCoordinates);
+}
+
+BOOST_AUTO_TEST_CASE (double_1)
+{
+    InputData data (sample_double_1);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::Duplicates);
+}
+
+BOOST_AUTO_TEST_CASE (double_2)
+{
+    InputData data (sample_double_2);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::Duplicates);
+}
+
+BOOST_AUTO_TEST_CASE (double_3)
+{
+    InputData data (sample_double_3);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::Duplicates);
+}
+
+BOOST_AUTO_TEST_CASE (in_holes)
+{
+    InputData data (sample_in_holes);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::BallsInHoles);
+}
+
+BOOST_AUTO_TEST_CASE (no_balls)
+{
+    InputData data (sample_no_balls);
+
+    BOOST_CHECK_EQUAL(data.GetDataStatus(), InputData::Status::NoBalls);
 }
